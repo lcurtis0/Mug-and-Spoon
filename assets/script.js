c5835ddaadd0e2e0fb07a2df8bc7ac1727b0f6e1
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-06-13 10:49:32 -0400

    added-pre-code

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,35 @@
-var genpassword [];
 
-var numbers [0, 1, 2, 3,];
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+
+
+
+  
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+var genpassword = [];
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var letters = ["a", "b" ,"c", "d" , "e" , "f" , "g", h, i, j, k, l ,m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~",];
+
+var x = numbers.random();
+var x = letters.random();
+var z = symbols.random();
+
+console.log(x, y, z)
 
 // var logicalOperators = ["and", "or", "not"];
